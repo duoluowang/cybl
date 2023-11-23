1d150ac2f82f8a795b0fa2aa7b45b8a0a44bc929
wangbin <309912361@qq.com>
2023-11-23 16:52:16 +0800

    storage封装

--- src/main.js
+++ src/main.js
@@ -6,11 +6,13 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import Welcome from './components/welcome.vue'
 import request from './utils/request'
+import storage from './utils/storage'
 
 
 
 console.log("环境变量=>",import.meta.env)
 const app = createApp(App)
 app.config.globalProperties.$request = request
+app.config.globalProperties.$storage = storage
 app.component('Welcome',Welcome)
 app.use(router).use(ElementPlus).mount('#app')
--- src/utils/storage.js
+++ src/utils/storage.js
@@ -7,7 +7,7 @@ export default {
     setItem(key, val) {
         let storage = this.getStroage();
         storage[key] = val;
-        window.localStorage.setItem(config.namespace, JSON.stringify(storage));
+        window.localStorage. setItem(config.namespace, JSON.stringify(storage));
     },
     getItem(key) {
         return this.getStroage()[key]
